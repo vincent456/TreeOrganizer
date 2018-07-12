1ed30e262960b39124fd03a8fcb4f2a10734fda5
Vincent Hudry <vincent.hudry@live.fr>
2018-07-12 14:25:29 +0200

    step in save tree state on quit

--- app/src/main/java/vincenthudry/organizer/Settings.java
+++ app/src/main/java/vincenthudry/organizer/Settings.java
@@ -1,5 +1,39 @@
 package vincenthudry.organizer;
 
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
 public class Settings {
     public static String databaseName="database.db";
+    public static void setTreeCheckpoint(Context context,long id){
+        File filesDir=context.getFilesDir();
+        File settings=new File(filesDir,"settings");
+        try (OutputStream outputStream=new FileOutputStream(settings)){
+
+            Properties properties=new Properties();
+            properties.setProperty("last tree",String.valueOf(id));
+            properties.store(outputStream,null);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+    public static long getTreeCheckpoint(Context context){
+        File filesDir=context.getFilesDir();
+        File settings=new File(filesDir,"settings");
+        try (InputStream inputStream=new FileInputStream(settings)){
+            Properties properties=new Properties();
+            properties.load(inputStream);
+            return Long.valueOf(properties.getProperty("last tree"));
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 }
